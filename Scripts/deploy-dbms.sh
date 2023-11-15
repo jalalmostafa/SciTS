110b415bd4660e3a419be816edcca2ed530b1a8e
Sandro <info@saninfo.de>
2023-11-15 13:05:48 +0100

    deploy

--- Scripts/deploy-dbms.sh
+++ Scripts/deploy-dbms.sh
@@ -72,13 +72,13 @@ curl https://clickhouse.com/ | sh
 
 
 
-sudo apt install -y gnupg postgresql postgresql-common apt-transport-https lsb-release wget
-sudo sh /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh  
-echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
-wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
-sudo apt install -y timescaledb-2-postgresql-14
-sudo apt-get install -y postgresql-client
-sudo systemctl restart postgresql
+# sudo apt install -y gnupg postgresql postgresql-common apt-transport-https lsb-release wget
+# sudo sh /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh  
+# echo "deb https://packagecloud.io/timescale/timescaledb/debian/ $(lsb_release -c -s) main" | sudo tee /etc/apt/sources.list.d/timescaledb.list
+# wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
+# sudo apt install -y timescaledb-2-postgresql-14
+# sudo apt-get install -y postgresql-client
+# sudo systemctl restart postgresql
 
 # TODO PG / TS TUNE
 
